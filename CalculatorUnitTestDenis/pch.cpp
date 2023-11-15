7588aa7fe866e0a7ae98fac41ce9e14ccbac4568
KARABAS228 <131360424+KARABAS228@users.noreply.github.com>
2023-11-15 18:41:43 +0500

    Я не понял как изменять файлы поэтому просто загружаю навые)

---
+++ CalculatorUnitTestDenis/pch.cpp
@@ -0,0 +1,53 @@
+﻿// pch.cpp: файл исходного кода, соответствующий предварительно скомпилированному заголовочному файлу
+
+#include "pch.h"
+
+// При использовании предварительно скомпилированных заголовочных файлов необходим следующий файл исходного кода для выполнения сборки.
+
+int Exponentiation(int num, int exponent)
+{
+	return (num*exponent);
+}
+int Exponentiation(int num, double exponent)
+{
+	return num*exponent;
+}
+double Exponentiation(double num, int exponent)
+{
+	return num*exponent;
+}
+double Exponentiation(double num, double exponent)
+{
+	return num*exponent;
+}
+
+int RemainderOfTheDivision(int num1, int num2)
+{
+	int a = num1 / num2;
+	double b = num1 / num2;
+	return b-a;
+}
+int RemainderOfTheDivision(int num1, double num2)
+{
+	int a = num1 / num2;
+	double b = num1 / num2;
+	return b - a;
+}
+int RemainderOfTheDivision(double num1, double num2)
+{
+	int a = num1 / num2;
+	double b = num1 / num2;
+	return b - a;
+}
+
+double Sqrt(int num)
+{
+	
+	return pow(num,0.5);
+}
+
+double Sqrt(double num)
+{
+	return pow(num, 0.5);;
+}
+
---
+++ CalculatorUnitTestDenis/pch.h
@@ -0,0 +1,25 @@
+﻿// pch.h: это предварительно скомпилированный заголовочный файл.
+// Перечисленные ниже файлы компилируются только один раз, что ускоряет последующие сборки.
+// Это также влияет на работу IntelliSense, включая многие функции просмотра и завершения кода.
+// Однако изменение любого из приведенных здесь файлов между операциями сборки приведет к повторной компиляции всех(!) этих файлов.
+// Не добавляйте сюда файлы, которые планируете часто изменять, так как в этом случае выигрыша в производительности не будет.
+
+#include<cmath>
+
+#ifndef PCH_H
+#define PCH_H
+
+// Добавьте сюда заголовочные файлы для предварительной компиляции
+int Exponentiation(int num, int exponent);
+int Exponentiation(int num, double exponent);
+double Exponentiation(double num, int exponent);
+double Exponentiation(double num, double exponent);
+
+int RemainderOfTheDivision(int num, int num2);
+int RemainderOfTheDivision(int num1, double num2);
+int RemainderOfTheDivision(double num1, double num2);
+
+double Sqrt(int num);
+double Sqrt(double num);
+
+#endif //PCH_H
